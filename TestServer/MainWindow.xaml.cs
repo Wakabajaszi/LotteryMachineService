ee614a3de8d7bc6918e8fa56f6b99278642e7a2b
Wakabajaszi <michal.jedrych@outlook.com>
2021-03-08 10:20:59 +0100

    Added TestServer to host Service

---
+++ TestServer/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using LotteryMachineService;
+
+
+
+
+namespace TestServer
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ServiceHost serviceHost;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            serviceHost = new ServiceHost(typeof(MemberService));
+            serviceHost.Open();
+            StartButton.IsEnabled = false;
+            EndButton.IsEnabled = true;
+            ServerStatusLabel.Content = "Server is working";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            serviceHost.Close();
+            EndButton.IsEnabled = false;
+            StartButton.IsEnabled = true;
+            ServerStatusLabel.Content = "";
+        }
+    }
+}
